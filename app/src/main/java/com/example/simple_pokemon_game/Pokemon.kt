bb0ca079aae314847c7e26634ddcdb830e9805b0
Pedestal19 <gabeoji01@gmail.com>
2020-04-14 21:56:15 +0100

    created pokemon object

--- app/src/main/java/com/example/simple_pokemon_game/Pokemon.kt
+++ app/src/main/java/com/example/simple_pokemon_game/Pokemon.kt
@@ -3,4 +3,22 @@ package com.example.simple_pokemon_game
 class Pokemon{
 
 
+    var name:String?=null
+    var desc:String?=null
+    var image:Int?=null
+    var power:Double?=null
+    var latitude:Double?=null
+    var longitude:Double?=null
+    var IsCatch:Boolean?=false
+
+    constructor(image:Int, name:String, desc:String, power:Double, latitude:Double, longitude:Double) {
+        this.name = name
+        this.desc = desc
+        this.image = image
+        this.power = power
+        this.latitude = latitude
+        this.longitude = longitude
+        this.IsCatch = false
+    }
+
 }
